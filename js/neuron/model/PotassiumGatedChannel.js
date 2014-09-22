d3f4b932878e458293ae6a9128768c0b22f89c98
ashrafabu <ashraf.abuthahir@gmail.com>
2014-09-22 11:23:26 +0530

    PotassiumGatedChannel Ported

--- js/neuron/model/PotassiumGatedChannel.js
+++ js/neuron/model/PotassiumGatedChannel.js
@@ -16,10 +16,31 @@ define( function( require ) {
   var NeuronConstants = require( 'NEURON/neuron/NeuronConstants' );
   var PieSliceShapedCaptureZone = require( 'NEURON/neuron/model/PieSliceShapedCaptureZone' );
   var ParticleType = require( 'NEURON/neuron/model/ParticleType' );
-
+  var NeuronSharedConstants = require( 'NEURON/neuron/common/NeuronSharedConstants' );
+  var MembraneCrossingDirection = require( 'NEURON/neuron/model/MembraneCrossingDirection' );
+  var MathUtils = require( 'NEURON/neuron/utils/MathUtils' );
 
   var CHANNEL_HEIGHT = NeuronConstants.MEMBRANE_THICKNESS * 1.2; // In nanometers.
   var CHANNEL_WIDTH = NeuronConstants.MEMBRANE_THICKNESS * 0.50; // In nanometers.
+
+  // Constants that control the rate at which this channel will capture ions
+  // when it is open.  Smaller numbers here will increase the capture rate
+  // and thus make the flow appear to be faster.
+  var MIN_INTER_CAPTURE_TIME = 0.00005; // In seconds of sim time.
+  var MAX_INTER_CAPTURE_TIME = 0.00020; // In seconds of sim time.
+
+  // Constant used when calculating how open this gate should be based on
+  // a value that exists within the Hodgkin-Huxley model.  This was
+  // empirically determined.
+  var N4_WHEN_FULLY_OPEN = 0.35;
+
+  // Delay range - used to make the timing of the instances of this gate
+  // vary a little bit in terms of when they open and close.
+  var MAX_STAGGER_DELAY = NeuronSharedConstants.MIN_ACTION_POTENTIAL_CLOCK_DT * 10; // In seconds of sim time.
+
+  var RAND = {nextDouble: function() {
+    return Math.random();
+  }};
 
   /**
    * @param {ParticleCapture} modelContainingParticles
@@ -29,6 +50,7 @@ define( function( require ) {
   function PotassiumGatedChannel( modelContainingParticles, hodgkinHuxleyModel ) {
     var thisChannel = this;
     GatedChannel.call( thisChannel, CHANNEL_WIDTH, CHANNEL_HEIGHT, modelContainingParticles );
+    thisChannel.staggerDelay = RAND.nextDouble() * MAX_STAGGER_DELAY;
     thisChannel.hodgkinHuxleyModel = hodgkinHuxleyModel;
     thisChannel.setInteriorCaptureZone( new PieSliceShapedCaptureZone( this.getCenterLocation(), CHANNEL_WIDTH * 5, Math.PI, Math.PI * 0.5 ) );
     thisChannel.reset();
@@ -36,7 +58,38 @@ define( function( require ) {
 
   return inherit( GatedChannel, PotassiumGatedChannel, {
     stepInTime: function( dt ) {
-      //TODO
+      GatedChannel.prototype.stepInTime.call( this, dt );
+      // Update the openness factor based on the state of the HH model.
+      // This is very specific to the model and the type of channel.  Note
+      // the non-linear mapping of conductance to the openness factor for
+      // the channels.  This is to make the gates appear to snap open and
+      // closed more rapidly, which was requested by the IPHY folks after
+      // seeing some demos.
+      var normalizedConductance =
+        Math.min( Math.abs( this.hodgkinHuxleyModel.get_delayed_n4( this.staggerDelay ) ) / N4_WHEN_FULLY_OPEN, 1 );
+      var openness = 1 - Math.pow( normalizedConductance - 1, 2 );
+      if ( openness > 0 && openness < 1 ) {
+        // Trim off some digits, otherwise we are continuously making
+        // tiny changes to this value due to internal gyrations of the
+        // HH model.
+        openness = MathUtils.round( openness, 2 );
+      }
+      if ( openness !== this.getOpenness() ) {
+        this.setOpenness( openness );
+        if ( this.isOpen() && this.getCaptureCountdownTimer() === Number.POSITIVE_INFINITY ) {
+          // We have just transitioned to the open state, so it is time
+          // to start capturing ions.
+          this.restartCaptureCountdownTimer( true );
+        }
+      }
+    },
+    //@Override
+    reset: function() {
+      GatedChannel.prototype.reset.call( this );
+      // Set up the capture time range, which will be used to control the
+      // rate of particle capture when this gate is open.
+      this.setMinInterCaptureTime( MIN_INTER_CAPTURE_TIME );
+      this.setMaxInterCaptureTime( MAX_INTER_CAPTURE_TIME );
     },
     getChannelColor: function() {
       return NeuronConstants.POTASSIUM_COLOR.colorUtilsDarker( 0.2 );
@@ -48,11 +101,9 @@ define( function( require ) {
       return ParticleType.POTASSIUM_ION;
     },
     chooseCrossingDirection: function() {
-      //TODO
-    },
-    restartCaptureCountdownTimer: function( captureNow ) {
-      //TODO
+      return MembraneCrossingDirection.IN_TO_OUT;
     }
+
   } );
 } );
 
@@ -72,20 +123,6 @@ define( function( require ) {
 //  // Class Data
 //  //----------------------------------------------------------------------------
 
-//  // Constants that control the rate at which this channel will capture ions
-//  // when it is open.  Smaller numbers here will increase the capture rate
-//  // and thus make the flow appear to be faster.
-//  private static final double MIN_INTER_CAPTURE_TIME = 0.00005; // In seconds of sim time.
-//  private static final double MAX_INTER_CAPTURE_TIME = 0.00020; // In seconds of sim time.
-//
-//  // Constant used when calculating how open this gate should be based on
-//  // a value that exists within the Hodgkin-Huxley model.  This was
-//  // empirically determined.
-//  private static final double N4_WHEN_FULLY_OPEN = 0.35;
-//
-//  // Delay range - used to make the timing of the instances of this gate
-//  // vary a little bit in terms of when they open and close.
-//  private static final double MAX_STAGGER_DELAY = NeuronDefaults.MIN_ACTION_POTENTIAL_CLOCK_DT * 10; // In seconds of sim time.
 //
 //  //----------------------------------------------------------------------------
 //  // Instance Data
@@ -109,62 +146,13 @@ define( function( require ) {
 //  //----------------------------------------------------------------------------
 //  // Methods
 //  //----------------------------------------------------------------------------
+
 //
-//  @Override
-//  public Color getChannelColor() {
-//    return ColorUtils.darkerColor(NeuronConstants.POTASSIUM_COLOR, 0.2);
-//  }
-//
-//  @Override
-//  public Color getEdgeColor() {
-//    return NeuronConstants.POTASSIUM_COLOR;
-//  }
-//
-//  @Override
-//  public void reset() {
-//    super.reset();
-//
-//    // Set up the capture time range, which will be used to control the
-//    // rate of particle capture when this gate is open.
-//    setMinInterCaptureTime(MIN_INTER_CAPTURE_TIME);
-//    setMaxInterCaptureTime(MAX_INTER_CAPTURE_TIME);
-//  }
+
 //
 //  @Override
 //  public void stepInTime(double dt) {
-//    super.stepInTime(dt);
-//    // Update the openness factor based on the state of the HH model.
-//    // This is very specific to the model and the type of channel.  Note
-//    // the non-linear mapping of conductance to the openness factor for
-//    // the channels.  This is to make the gates appear to snap open and
-//    // closed more rapidly, which was requested by the IPHY folks after
-//    // seeing some demos.
-//    double normalizedConductance =
-//           Math.min(Math.abs(hodgkinHuxleyModel.get_delayed_n4(staggerDelay))/N4_WHEN_FULLY_OPEN, 1);
-//    double openness = 1 - Math.pow(normalizedConductance - 1, 2);
-//    if (openness > 0 && openness < 1){
-//      // Trim off some digits, otherwise we are continuously making
-//      // tiny changes to this value due to internal gyrations of the
-//      // HH model.
-//      openness = MathUtils.round(openness, 2);
-//    }
-//    if (openness != getOpenness()){
-//      setOpenness(openness);
-//      if (isOpen() && getCaptureCountdownTimer() == Double.POSITIVE_INFINITY){
-//        // We have just transitioned to the open state, so it is time
-//        // to start capturing ions.
-//        restartCaptureCountdownTimer(true);
-//      }
-//    }
+
 //  }
-//
-//  @Override
-//  protected ParticleType getParticleTypeToCapture() {
-//    return ParticleType.POTASSIUM_ION;
-//  }
-//
-//  @Override
-//  protected MembraneCrossingDirection chooseCrossingDirection() {
-//    return MembraneCrossingDirection.IN_TO_OUT;
-//  }
+
 //}
