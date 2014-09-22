ff00557f30cf87f4f43df79476b4bb64dff722e0
ashrafabu <ashraf.abuthahir@gmail.com>
2014-09-22 10:18:00 +0530

    DualGateCha.. and SodisumDua..Chanel ported

--- js/neuron/model/SodiumDualGatedChannel.js
+++ js/neuron/model/SodiumDualGatedChannel.js
@@ -16,11 +16,49 @@ define( function( require ) {
   var GatedChannel = require( 'NEURON/neuron/model/GatedChannel' );
   var NeuronConstants = require( 'NEURON/neuron/NeuronConstants' );
   var PieSliceShapedCaptureZone = require( 'NEURON/neuron/model/PieSliceShapedCaptureZone' );
+  var MembraneCrossingDirection = require( 'NEURON/neuron/model/MembraneCrossingDirection' );
+  var DualGateChannelTraversalMotionStrategy = require( 'NEURON/neuron/model/DualGateChannelTraversalMotionStrategy' );
   var ParticleType = require( 'NEURON/neuron/model/ParticleType' );
-
+  var NeuronSharedConstants = require( 'NEURON/neuron/common/NeuronSharedConstants' );
+  var MathUtils = require( 'NEURON/neuron/utils/MathUtils' );
 
   var CHANNEL_HEIGHT = NeuronConstants.MEMBRANE_THICKNESS * 1.2; // In nanometers.
   var CHANNEL_WIDTH = NeuronConstants.MEMBRANE_THICKNESS * 0.50; // In nanometers.
+
+  //
+  // Constant used when calculating how open this gate should be based on
+  // a value that exists within the Hodgkin-Huxley model.  This was
+  // empirically determined.
+  var M3H_WHEN_FULLY_OPEN = 0.25;
+
+  // Possible values for internal state.
+  var GateState = Object.freeze( {
+    IDLE: 'IDLE', OPENING: 'OPENING', BECOMING_INACTIVE: 'BECOMING_INACTIVE', INACTIVATED: 'INACTIVATED', RESETTING: 'RESETTING'
+  } );
+
+  var RAND = {nextDouble: function() {
+    return Math.random();
+  }};
+
+
+  // Values used for deciding on state transitions.  These were empirically
+  // determined.
+  var ACTIVATION_DECISION_THRESHOLD = 0.002;
+  var FULLY_INACTIVE_DECISION_THRESHOLD = 0.98;
+
+  // Values used for timed state transitions.
+  var INACTIVE_TO_RESETTING_TIME = 0.001; // In seconds of sim time.
+  var RESETTING_TO_IDLE_TIME = 0.001; // In seconds of sim time.
+
+// Constants that control the rate at which this channel will capture ions
+// when it is open.  Smaller numbers here will increase the capture rate
+// and thus make the flow appear to be faster.
+  var MIN_INTER_CAPTURE_TIME = 0.00002; // In seconds of sim time.
+  var MAX_INTER_CAPTURE_TIME = 0.00010; // In seconds of sim time.
+
+  // Delay range - used to make the timing of the instances of this gate
+  // vary a little bit in terms of when they open and close.
+  var MAX_STAGGER_DELAY = NeuronSharedConstants.MIN_ACTION_POTENTIAL_CLOCK_DT * 5; // In seconds of sim time.
 
 
   /**
@@ -32,15 +70,133 @@ define( function( require ) {
   function SodiumDualGatedChannel( modelContainingParticles, hodgkinHuxleyModel ) {
     var thisChannel = this;
     GatedChannel.call( thisChannel, CHANNEL_WIDTH, CHANNEL_HEIGHT, modelContainingParticles );
-    this.hodgkinHuxleyModel = hodgkinHuxleyModel;
+    thisChannel.gateState = GateState.IDLE;
+    thisChannel.hodgkinHuxleyModel = hodgkinHuxleyModel;
+    thisChannel.stateTransitionTimer = 0;
+    thisChannel.staggerDelay = 0;
+    this.previousNormalizedConductance = 0;
     thisChannel.setExteriorCaptureZone( new PieSliceShapedCaptureZone( thisChannel.getCenterLocation(), CHANNEL_WIDTH * 5, 0, Math.PI * 0.7 ) );
     thisChannel.reset();
   }
 
   return inherit( GatedChannel, SodiumDualGatedChannel, {
+
+
     stepInTime: function( dt ) {
-      //TODO
+      GatedChannel.prototype.stepInTime.call( this, dt );
+
+      // Get the conductance and normalize it from 0 to 1.
+      var normalizedConductance = this.calculateNormalizedConductance();
+
+      if ( normalizedConductance >= 0 && normalizedConductance <= 1 ) {
+        // Trim off some digits to limit very small changes.
+        normalizedConductance = MathUtils.round( normalizedConductance, 4 );
+      }
+      else {
+        // This shouldn't happen, debug it if it does.
+        console.log( "Sodium Dual  Channel Normalized conductance out of range, = " + normalizedConductance );
+        assert && assert( false );
+      }
+
+      // Update the state.
+      switch( this.gateState ) {
+        case GateState.IDLE:
+          if ( normalizedConductance > ACTIVATION_DECISION_THRESHOLD ) {
+            // We are opening, change to the appropriate state.
+            this.setOpenness( this.mapOpennessToNormalizedConductance( normalizedConductance ) );
+            this.gateState = GateState.OPENING;
+          }
+          break;
+
+        case GateState.OPENING:
+          if ( this.isOpen() && this.getCaptureCountdownTimer() === Number.POSITIVE_INFINITY ) {
+            // We are open enough to start capturing particles.
+            this.restartCaptureCountdownTimer( true );
+          }
+          if ( this.previousNormalizedConductance > normalizedConductance ) {
+            // We are on the way down, so set a new state.
+            this.gateState = GateState.BECOMING_INACTIVE;
+            // Should be fully open at this point.
+            this.setOpenness( 1 );
+          }
+          else {
+            // Set the openness based on the normalized conductance value.
+            // Note the non-linear mapping.  This was done to make them
+            // appear to be fully open earlier in the action potential,
+            // which was requested by the IPHY folks.
+            this.setOpenness( this.mapOpennessToNormalizedConductance( normalizedConductance ) );
+          }
+          break;
+
+        case GateState.BECOMING_INACTIVE:
+          if ( this.getInactivationAmt() < FULLY_INACTIVE_DECISION_THRESHOLD ) {
+            // Not yet fully inactive - update the level.  Note the non-
+            // linear mapping to the conductance amount.
+            this.setInactivationAmt( 1 - Math.pow( normalizedConductance, 7 ) );
+          }
+          else {
+            // Fully inactive, move to next state.
+            this.setInactivationAmt( 1 );
+            this.gateState = GateState.INACTIVATED;
+            this.stateTransitionTimer = INACTIVE_TO_RESETTING_TIME;
+          }
+          break;
+
+        case GateState.INACTIVATED:
+          this.stateTransitionTimer -= dt;
+          if ( this.stateTransitionTimer < 0 ) {
+            // Time to start resetting.
+            this.gateState = GateState.RESETTING;
+            this.stateTransitionTimer = RESETTING_TO_IDLE_TIME;
+          }
+          break;
+
+        case GateState.RESETTING:
+          this.stateTransitionTimer -= dt;
+          if ( this.stateTransitionTimer >= 0 ) {
+            // Move the values of openness and activation back towards
+            // their idle (i.e. resting) states.  The mapping of the
+            // inactivation amount as a function of time is very non-
+            // linear.  This is because the IPHY people requested that
+            // the "little ball doesn't pop out" until the the gate has
+            // closed up.
+            this.setOpenness( 1 - Math.pow( this.stateTransitionTimer / RESETTING_TO_IDLE_TIME - 1, 10 ) );
+            this.setInactivationAmt( 1 - Math.pow( this.stateTransitionTimer / RESETTING_TO_IDLE_TIME - 1, 20 ) );
+          }
+          else {
+            // Go back to the idle, or resting, state.
+            this.setOpenness( 0 );
+            this.setInactivationAmt( 0 );
+            this.updateStaggerDelay();
+            this.gateState = GateState.IDLE;
+          }
+          break;
+      }
+
+      // Save values for the next time through.
+      this.previousNormalizedConductance = normalizedConductance;
     },
+
+    //@Override
+    reset: function() {
+      GatedChannel.prototype.reset.call( this );
+
+      // Set up the capture time range, which will be used to control the
+      // rate of particle capture when this gate is open.
+      this.setMinInterCaptureTime( MIN_INTER_CAPTURE_TIME );
+      this.setMaxInterCaptureTime( MAX_INTER_CAPTURE_TIME );
+
+      // Initialize some internal state.
+      this.gateState = GateState.IDLE;
+      this.stateTransitionTimer = 0;
+      if ( this.hodgkinHuxleyModel !== null ) {
+        this.previousNormalizedConductance = this.calculateNormalizedConductance();
+      }
+
+      // Initialize the stagger delay.
+      this.updateStaggerDelay();
+    },
+
     getChannelColor: function() {
       return NeuronConstants.SODIUM_COLOR.colorUtilsDarker( 0.2 );
     },
@@ -50,16 +206,31 @@ define( function( require ) {
     getParticleTypeToCapture: function() {
       return ParticleType.SODIUM_ION;
     },
+    updateStaggerDelay: function() {
+      this.staggerDelay = RAND.nextDouble() * MAX_STAGGER_DELAY;
+    },
+    //@Override
     chooseCrossingDirection: function() {
-      //TODO
+      return MembraneCrossingDirection.OUT_TO_IN;
     },
-    restartCaptureCountdownTimer: function( captureNow ) {
-      //TODO
-    },
+
     //@Override This membrane channel has an inactivation gate.
     getHasInactivationGate: function() {
       return true;
+    },
+    //@Override
+    moveParticleThroughNeuronMembrane: function( particle, maxVelocity ) {
+      particle.setMotionStrategy( new DualGateChannelTraversalMotionStrategy( this, particle.getPositionReference() ) );
+    },
+
+    mapOpennessToNormalizedConductance: function( normalizedConductance ) {
+      assert && assert( normalizedConductance >= 0 && normalizedConductance <= 1 );
+      return 1 - Math.pow( normalizedConductance - 1, 20 );
+    },
+    calculateNormalizedConductance: function() {
+      return Math.min( Math.abs( this.hodgkinHuxleyModel.get_delayed_m3h( this.staggerDelay ) ) / M3H_WHEN_FULLY_OPEN, 1 );
     }
+
   } );
 
 } );
@@ -99,34 +270,7 @@ define( function( require ) {
 //  //----------------------------------------------------------------------------
 //
 
-//
-//  // Constant used when calculating how open this gate should be based on
-//  // a value that exists within the Hodgkin-Huxley model.  This was
-//  // empirically determined.
-//  private static final double M3H_WHEN_FULLY_OPEN = 0.25;
-//
-//  // Possible values for internal state.
-//  private enum GateState {IDLE, OPENING, BECOMING_INACTIVE, INACTIVATED, RESETTING};
-//
-//  // Values used for deciding on state transitions.  These were empirically
-//  // determined.
-//  private static final double ACTIVATION_DECISION_THRESHOLD = 0.002;
-//  private static final double FULLY_INACTIVE_DECISION_THRESHOLD = 0.98;
-//
-//  // Values used for timed state transitions.
-//  private static final double INACTIVE_TO_RESETTING_TIME = 0.001; // In seconds of sim time.
-//  private static final double RESETTING_TO_IDLE_TIME = 0.001; // In seconds of sim time.
-//
-// Constants that control the rate at which this channel will capture ions
-// when it is open.  Smaller numbers here will increase the capture rate
-// and thus make the flow appear to be faster.
-//private static final double MIN_INTER_CAPTURE_TIME = 0.00002; // In seconds of sim time.
-//private static final double MAX_INTER_CAPTURE_TIME = 0.00010; // In seconds of sim time.
 
-//  // Delay range - used to make the timing of the instances of this gate
-//  // vary a little bit in terms of when they open and close.
-//  private static final double MAX_STAGGER_DELAY = NeuronDefaults.MIN_ACTION_POTENTIAL_CLOCK_DT * 5; // In seconds of sim time.
-//
 //  //----------------------------------------------------------------------------
 //  // Instance Data
 //  //----------------------------------------------------------------------------
@@ -153,154 +297,17 @@ define( function( require ) {
 //  //----------------------------------------------------------------------------
 //  // Methods
 //  //----------------------------------------------------------------------------
-//
-//  @Override
-//  public Color getChannelColor() {
-//    return ColorUtils.darkerColor(NeuronConstants.SODIUM_COLOR, 0.2);
-//  }
-//
-//  @Override
-//  public Color getEdgeColor() {
-//    return NeuronConstants.SODIUM_COLOR;
-//  }
+
 //
 //  @Override
 //  public void stepInTime(double dt) {
 //
-//    super.stepInTime(dt);
-//
-//    // Get the conductance and normalize it from 0 to 1.
-//    double normalizedConductance = calculateNormalizedConductance();
-//
-//    if (normalizedConductance >= 0 && normalizedConductance <= 1){
-//      // Trim off some digits to limit very small changes.
-//      normalizedConductance = MathUtils.round(normalizedConductance, 4);
-//    }
-//    else{
-//      // This shouldn't happen, debug it if it does.
-//      System.err.println(getClass().getName() + "Nomalized conductance out of range, = " + normalizedConductance);
-//      assert false;
-//    }
-//
-//    // Update the state.
-//    switch ( gateState ) {
-//      case IDLE:
-//        if (normalizedConductance > ACTIVATION_DECISION_THRESHOLD){
-//          // We are opening, change to the appropriate state.
-//          setOpenness(mapOpennessToNormalizedConductance( normalizedConductance ));
-//          gateState = GateState.OPENING;
-//        }
-//        break;
-//
-//      case OPENING:
-//        if (isOpen() && getCaptureCountdownTimer() == Double.POSITIVE_INFINITY){
-//          // We are open enough to start capturing particles.
-//          restartCaptureCountdownTimer(true);
-//        }
-//        if (previousNormalizedConductance  > normalizedConductance){
-//          // We are on the way down, so set a new state.
-//          gateState = GateState.BECOMING_INACTIVE;
-//          // Should be fully open at this point.
-//          setOpenness(1);
-//        }
-//        else{
-//          // Set the openness based on the normalized conductance value.
-//          // Note the non-linear mapping.  This was done to make them
-//          // appear to be fully open earlier in the action potential,
-//          // which was requested by the IPHY folks.
-//          setOpenness(mapOpennessToNormalizedConductance( normalizedConductance ));
-//        }
-//        break;
-//
-//      case BECOMING_INACTIVE:
-//        if (getInactivationAmt() < FULLY_INACTIVE_DECISION_THRESHOLD){
-//          // Not yet fully inactive - update the level.  Note the non-
-//          // linear mapping to the conductance amount.
-//          setInactivationAmt(1 - Math.pow(normalizedConductance, 7));
-//        }
-//        else{
-//          // Fully inactive, move to next state.
-//          setInactivationAmt(1);
-//          gateState = GateState.INACTIVATED;
-//          stateTransitionTimer = INACTIVE_TO_RESETTING_TIME;
-//        }
-//        break;
-//
-//      case INACTIVATED:
-//        stateTransitionTimer -= dt;
-//        if (stateTransitionTimer < 0){
-//          // Time to start resetting.
-//          gateState = GateState.RESETTING;
-//          stateTransitionTimer = RESETTING_TO_IDLE_TIME;
-//        }
-//        break;
-//
-//      case RESETTING:
-//        stateTransitionTimer -= dt;
-//        if (stateTransitionTimer >= 0){
-//          // Move the values of openness and activation back towards
-//          // their idle (i.e. resting) states.  The mapping of the
-//          // inactivation amount as a function of time is very non-
-//          // linear.  This is because the IPHY people requested that
-//          // the "little ball doesn't pop out" until the the gate has
-//          // closed up.
-//          setOpenness(1 - Math.pow(stateTransitionTimer/RESETTING_TO_IDLE_TIME - 1, 10));
-//          setInactivationAmt(1 - Math.pow(stateTransitionTimer/RESETTING_TO_IDLE_TIME - 1, 20));
-//        }
-//        else{
-//          // Go back to the idle, or resting, state.
-//          setOpenness(0);
-//          setInactivationAmt(0);
-//          updateStaggerDelay();
-//          gateState = GateState.IDLE;
-//        }
-//        break;
-//    }
-//
-//    // Save values for the next time through.
-//    previousNormalizedConductance = normalizedConductance;
-//  }
-//
-//  @Override
-//  public void moveParticleThroughNeuronMembrane(Particle particle, double maxVelocity) {
-//    particle.setMotionStrategy(new DualGateChannelTraversalMotionStrategy(this, particle.getPositionReference()));
-//  }
-//
-//  private double mapOpennessToNormalizedConductance(double normalizedConductance){
-//    assert normalizedConductance >= 0 && normalizedConductance <= 1;
-//    return 1 - Math.pow(normalizedConductance - 1, 20);
+
 //  }
 //
 
-//
-//  private double calculateNormalizedConductance(){
-//    return Math.min(Math.abs(hodgkinHuxleyModel.get_delayed_m3h(staggerDelay))/M3H_WHEN_FULLY_OPEN, 1);
-//  }
-//
-//  @Override
-//  protected ParticleType getParticleTypeToCapture() {
-//    return ParticleType.SODIUM_ION;
-//  }
-//
-//  @Override
-//  public void reset() {
-//    super.reset();
-//
-//    // Set up the capture time range, which will be used to control the
-//    // rate of particle capture when this gate is open.
-//    setMinInterCaptureTime(MIN_INTER_CAPTURE_TIME);
-//    setMaxInterCaptureTime(MAX_INTER_CAPTURE_TIME);
-//
-//    // Initialize some internal state.
-//    gateState = GateState.IDLE;
-//    stateTransitionTimer = 0;
-//    if (hodgkinHuxleyModel != null){
-//      previousNormalizedConductance = calculateNormalizedConductance();
-//    }
-//
-//    // Initialize the stagger delay.
-//    updateStaggerDelay();
-//  }
+
+
 //
 //  //----------------------------------------------------------------------------
 //  // Test Harness
--- js/neuron/model/SpeedChangeLinearMotionStrategy.js
+++ js/neuron/model/SpeedChangeLinearMotionStrategy.js
@@ -1,40 +1,42 @@
-//// Copyright 2002-2012, University of Colorado
-//
-//package edu.colorado.phet.neuron.model;
-//
-//import edu.colorado.phet.common.phetcommon.math.vector.MutableVector2D;
-//
-///**
-// * This is a very specialized motion strategy that is basically a linear
-// * motion but that starts at one speed and then changes to another.  It was
-// * created for a very specific application - getting particles to move quickly
-// * away from the exit of a channel with an inactivation gate, and then slowing
-// * down.  It may have other applications.
-// *
-// * @author John Blanco
-// */
-//public class SpeedChangeLinearMotionStrategy extends MotionStrategy {
-//
-//  private MutableVector2D velocityVector = new MutableVector2D();
-//  private double firstSpeedCountdownTimer = 0;
-//  private final double speedScaleFactor;
-//
-//  public SpeedChangeLinearMotionStrategy( MutableVector2D initialVelocity, double speedScaleFactor, double timeAtFirstSpeed ) {
-//    this.velocityVector.setComponents( initialVelocity.getX(), initialVelocity.getY() );
-//    this.speedScaleFactor = speedScaleFactor;
-//    this.firstSpeedCountdownTimer = timeAtFirstSpeed;
-//  }
-//
-//  @Override
-//  public void move( IMovable movable, IFadable fadableModelElement, double dt ) {
-//    movable.setPosition( movable.getPosition().getX() + velocityVector.getX() * dt,
-//        movable.getPosition().getY() + velocityVector.getY() * dt );
-//    if ( firstSpeedCountdownTimer > 0 ) {
-//      firstSpeedCountdownTimer -= dt;
-//      if ( firstSpeedCountdownTimer <= 0 ) {
-//        // Scale the speed.
-//        velocityVector.scale( speedScaleFactor );
-//      }
-//    }
-//  }
-//}
+//  Copyright 2002-2014, University of Colorado Boulder
+/**
+ * This is a very specialized motion strategy that is basically a linear
+ * motion but that starts at one speed and then changes to another.  It was
+ * created for a very specific application - getting particles to move quickly
+ * away from the exit of a channel with an inactivation gate, and then slowing
+ * down.  It may have other applications.
+ *
+ * @author John Blanco
+ * @author Sharfudeen Ashraf (for Ghent University)
+ */
+define( function( require ) {
+  'use strict';
+
+  //imports
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Vector2 = require( 'DOT/Vector2' );
+  var MotionStrategy = require( 'NEURON/neuron/model/MotionStrategy' );
+
+  function SpeedChangeLinearMotionStrategy( initialVelocity, speedScaleFactor, timeAtFirstSpeed ) {
+    this.velocityVector = new Vector2( initialVelocity.x, initialVelocity.y );
+    this.speedScaleFactor = speedScaleFactor;
+    this.firstSpeedCountdownTimer = timeAtFirstSpeed;
+  }
+
+  return inherit( MotionStrategy, SpeedChangeLinearMotionStrategy, {
+    move: function( movable, fadableModelElement, dt ) {
+      movable.setPosition( movable.getPosition().x + this.velocityVector.x * dt,
+          movable.getPosition().y + this.velocityVector.y * dt );
+      if ( this.firstSpeedCountdownTimer > 0 ) {
+        this.firstSpeedCountdownTimer -= dt;
+        if ( this.firstSpeedCountdownTimer <= 0 ) {
+          // Scale the speed.
+          this.velocityVector.multiplyScalar( this.speedScaleFactor );
+        }
+      }
+    }
+  } );
+} );
+
+
+
