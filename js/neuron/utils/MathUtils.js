5d5c187b36cd8920aebb59172c990f396f143a69
ashraf <ashraf.abuthahir@gmail.com>
2014-12-08 19:13:49 +0530

    Constructor Removed From MathUtils Classs issue #40

--- js/neuron/utils/MathUtils.js
+++ js/neuron/utils/MathUtils.js
@@ -15,42 +15,39 @@ define( function( require ) {
   var distanceCalculatorVectorLHS = new Vector2();
   var distanceCalculatorVectorRHS = new Vector2();
 
-  function MathUtils() {
+  var MathUtils = {
+    /**
+     * A method to calculate distance by reusing vector instances.
+     * This method is created to reduce Vector2 instance allocation during distance calcualtion
+     * @param {number} posX
+     * @param {number} posY
+     * @param {number} otherPosX
+     * @param {number} otherPosY
+     * @returns {number}
+     */
+    distanceBetween: function( posX, posY, otherPosX, otherPosY ) {
+      distanceCalculatorVectorLHS.x = posX;
+      distanceCalculatorVectorLHS.y = posY;
+      distanceCalculatorVectorRHS.x = otherPosX;
+      distanceCalculatorVectorRHS.y = otherPosY;
+      return distanceCalculatorVectorLHS.distance( distanceCalculatorVectorRHS );
+    },
 
-  }
+    round: function( val, places ) {
+      var factor = Math.pow( 10, places );
 
-  /**
-   * A method to calculate distance by reusing vector instances.
-   * This method is created to reduce Vector2 instance allocation during distance calcualtion
-   * @param {number} posX
-   * @param {number} posY
-   * @param {number} otherPosX
-   * @param {number} otherPosY
-   * @returns {number}
-   */
-  MathUtils.distanceBetween = function( posX, posY, otherPosX, otherPosY ) {
-    distanceCalculatorVectorLHS.x = posX;
-    distanceCalculatorVectorLHS.y = posY;
-    distanceCalculatorVectorRHS.x = otherPosX;
-    distanceCalculatorVectorRHS.y = otherPosY;
-    return distanceCalculatorVectorLHS.distance( distanceCalculatorVectorRHS );
+      // Shift the decimal the correct number of places
+      // to the right.
+      val = val * factor;
+
+      // Round to the nearest integer.
+      var tmp = Math.round( val );
+
+      // Shift the decimal the correct number of places
+      // back to the left.
+      return tmp / factor;
+    }
   };
-
-  MathUtils.round = function( val, places ) {
-    var factor = Math.pow( 10, places );
-
-    // Shift the decimal the correct number of places
-    // to the right.
-    val = val * factor;
-
-    // Round to the nearest integer.
-    var tmp = Math.round( val );
-
-    // Shift the decimal the correct number of places
-    // back to the left.
-    return tmp / factor;
-  };
-
   return MathUtils;
 } );
 
