b2afbba5c7ea83a2b25965864b1baac9820d6b9e
jbphet <john.blanco@colorado.edu>
2015-09-16 14:08:24 -0600

    changed to use Circle instead of Path and Shape

--- js/neuron/chart/view/GrippyIndentNode.js
+++ js/neuron/chart/view/GrippyIndentNode.js
@@ -1,11 +1,10 @@
 // Copyright 2002-2011, University of Colorado
 /**
  /**
- * This node is meant to portray a small round indentation on a surface.  This
- * is a modern user interface paradigm that is intended to convey the concept
- * of "gripability" (sp?), i.e. something that the user can click on and
- * subsequently grab.  This is meant to look somewhat 3D, much like etched
- * borders do.
+ * This node is meant to portray a small round indentation on a surface.  This is a modern user interface paradigm that
+ * is intended to convey the concept of "gripability" (sp?), i.e. something that the user can click on and subsequently
+ * grab.  This is meant to look somewhat 3D, much like etched borders do.
+ *
  * @author John Blanco
  * @author Sharfudeen Ashraf (for Ghent University)
  */
@@ -13,11 +12,13 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var inherit = require( 'PHET_CORE/inherit' );
+  var Circle = require( 'SCENERY/nodes/Circle' );
   var Color = require( 'SCENERY/util/Color' );
+  var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
-  var Path = require( 'SCENERY/nodes/Path' );
-  var Shape = require( 'KITE/Shape' );
+
+  // constants
+  var STROKE_LINE_WIDTH = 0.5;
 
   /**
    *
@@ -27,22 +28,21 @@ define( function( require ) {
    */
   function GrippyIndentNode( diameter, baseColor ) {
 
-    var thisNode = this;
-    Node.call( thisNode );
+    Node.call( this );
     var baseDarkerColor = baseColor.darkerColor( 0.9 );
     var translucentDarkerColor = new Color( baseDarkerColor.getRed(), baseDarkerColor.getGreen(),
       baseDarkerColor.getBlue(), baseColor.getAlpha() );
     var baseLighterColor = baseColor.brighterColor( 0.9 );
     var translucentBrighterColor = new Color( baseLighterColor.getRed(), baseLighterColor.getGreen(),
       baseLighterColor.getBlue(), baseColor.getAlpha() );
-    var radius = diameter / 2;
-    thisNode.addChild( new Path( new Shape().circle( 0, 0, radius, radius ), { fill: translucentBrighterColor } ) );
-    var offsetFactor = 0.8;
-    thisNode.addChild( new Path( new Shape().circle( 0, 0,
-      radius * offsetFactor, radius * offsetFactor ), { fill: translucentDarkerColor } ) );
+    var radius = diameter / 2 - STROKE_LINE_WIDTH;
 
+    this.addChild( new Circle( radius, {
+      fill: translucentDarkerColor,
+      stroke: translucentBrighterColor,
+      lineWidth: STROKE_LINE_WIDTH
+    } ) );
   }
-
 
   return inherit( Node, GrippyIndentNode );
 
