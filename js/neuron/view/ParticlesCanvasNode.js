852341a929773a59629710db4629b542fafbf99a
jbphet <john.blanco@colorado.edu>
2015-12-21 16:01:20 -0700

    refactored ParticlesCanvasNode.paintCanvas in response to a TODO item marked during code review, see #97

--- js/neuron/view/ParticlesCanvasNode.js
+++ js/neuron/view/ParticlesCanvasNode.js
@@ -42,78 +42,77 @@ define( function( require ) {
 
   return inherit( CanvasNode, ParticlesCanvasNode, {
 
+    // @private
+    renderSodiumParticles: function( particles, context ) {
+      var self = this;
+      context.fillStyle = particles[ 0 ].getRepresentationColor().getCanvasStyle(); // All sodium ions are of the same color,
+      var transformedRadius = self.modelViewTransform.modelToViewDeltaX( particles[ 0 ].getRadius() );
+      context.lineWidth = 0.3;
+      context.strokeStyle = 'black';
+      particles.forEach( function( particle ) {
+        context.globalAlpha = particle.getOpacity();
+        context.beginPath();
+        var x = self.modelViewTransform.modelToViewX( particle.getPositionX() );
+        var y = self.modelViewTransform.modelToViewY( particle.getPositionY() );
+        context.arc( x, y, transformedRadius, 0, 2 * Math.PI, true );
+        context.closePath();
+        context.stroke();
+        context.fill();
+      } );
+    },
+
+    renderPotassiumParticles: function( particles, context ) {
+      var self = this;
+      context.fillStyle = particles[ 0 ].getRepresentationColor().getCanvasStyle();
+      var size = self.modelViewTransform.modelToViewDeltaX( particles[ 0 ].getRadius() * 2 ) * 0.55;
+      context.lineWidth = 0.3;
+      context.strokeStyle = 'black';
+      particles.forEach( function( particle ) {
+        context.globalAlpha = particle.getOpacity();
+        context.beginPath();
+        var x = self.modelViewTransform.modelToViewX( particle.getPositionX() );
+        var y = self.modelViewTransform.modelToViewY( particle.getPositionY() );
+        context.moveTo( x - size, y );
+        context.lineTo( x, y - size );
+        context.lineTo( x + size, y );
+        context.lineTo( x, y + size );
+        context.closePath();
+        context.stroke();
+        context.fill();
+      } );
+    },
+
+    // @private
+    renderParticles: function( particles, context ) {
+      var self = this;
+      console.log( 'here I am boss!' );
+
+      // group by particle type, this way no need to set the fillStyle for every particle instance
+      var particlesGroupedByType = _.groupBy( particles, function( particle ) {
+        return particle.getType();
+      } );
+
+      _.forOwn( particlesGroupedByType, function( particlesOfSameType, particleType ) {
+        switch( particleType ) {
+          case ParticleType.SODIUM_ION:
+            self.renderSodiumParticles( particlesOfSameType, context );
+            break;
+          case ParticleType.POTASSIUM_ION:
+            self.renderPotassiumParticles( particlesOfSameType, context );
+            break;
+        }
+      } );
+    },
+
     /**
      * @param {CanvasRenderingContext2D} context
      * @override
      * @public
      */
     paintCanvas: function( context ) {
-
-      var thisNode = this;
-      var canvasStrokeStyle = Color.BLACK.getCanvasStyle();
-
-      // TODO: This appears to be redefining the function on every paint, which seems crazy.  Can this be moved out?
-      function renderParticles( particleTypes ) {
-
-        // group by particle type, this way no need to set the fillStyle for every particle instance
-        var particlesGroupedByType = _.groupBy( particleTypes, function( particle ) {
-          return particle.getType();
-        } );
-
-        _.forOwn( particlesGroupedByType, function( particlesOfSameType, particleType ) {
-          switch( particleType ) {
-            case ParticleType.SODIUM_ION:
-              renderSodiumParticles( particlesOfSameType );
-              //renderSodiumParticles( particlesOfSameType );
-              break;
-            case ParticleType.POTASSIUM_ION:
-              renderPotassiumParticles( particlesOfSameType );
-              //renderPotassiumParticles( particlesOfSameType );
-              break;
-          }
-        } );
-
-        function renderSodiumParticles( particles ) {
-          context.fillStyle = particles[ 0 ].getRepresentationColor().getCanvasStyle();// All sodium ions are of the same color,
-          var transformedRadius = thisNode.modelViewTransform.modelToViewDeltaX( particles[ 0 ].getRadius() );
-          context.lineWidth = 0.2;
-          context.strokeStyle = canvasStrokeStyle;
-          particles.forEach( function( particle ) {
-            context.globalAlpha = particle.getOpacity();
-            context.beginPath();
-            var x = thisNode.modelViewTransform.modelToViewX( particle.getPositionX() );
-            var y = thisNode.modelViewTransform.modelToViewY( particle.getPositionY() );
-            context.arc( x, y, transformedRadius, 0, 2 * Math.PI, true );
-            context.closePath();
-            context.stroke();
-            context.fill();
-          } );
-        }
-
-        function renderPotassiumParticles( particles ) {
-          context.fillStyle = particles[ 0 ].getRepresentationColor().getCanvasStyle();
-          var size = thisNode.modelViewTransform.modelToViewDeltaX( particles[ 0 ].getRadius() * 2 ) * 0.75;//was 0.85
-          context.lineWidth = 0.2;
-          context.strokeStyle = canvasStrokeStyle;
-          particles.forEach( function( particle ) {
-            context.globalAlpha = particle.getOpacity();
-            context.beginPath();
-            var x = thisNode.modelViewTransform.modelToViewX( particle.getPositionX() );
-            var y = thisNode.modelViewTransform.modelToViewY( particle.getPositionY() );
-            context.moveTo( x - size, y );
-            context.lineTo( x, y - size );
-            context.lineTo( x + size, y );
-            context.lineTo( x, y + size );
-            context.closePath();
-            context.stroke();
-            context.fill();
-          } );
-        }
-      }
-
-      renderParticles( this.neuronModel.backgroundParticles.getArray() );
-      renderParticles( this.neuronModel.transientParticles.getArray() );
-      renderParticles( this.neuronModel.playbackParticles.getArray() );
+      this.renderParticles( this.neuronModel.backgroundParticles.getArray(), context );
+      this.renderParticles( this.neuronModel.transientParticles.getArray(), context );
+      this.renderParticles( this.neuronModel.playbackParticles.getArray(), context );
     }
 
   } );
