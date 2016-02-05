b0f85dace6f70a2ac7f14eb50d52cca6bbf5e33f
jbphet <john.blanco@colorado.edu>
2016-02-05 12:32:10 -0700

    backed out addition of TANDEM to list of libs - @samreid is going to change things so that this isn't needed, at least for now

--- js/neuron-config.js
+++ js/neuron-config.js
@@ -35,7 +35,6 @@ require.config( {
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
-    TANDEM: '../../tandem/js',
 
     // this sim
     NEURON: '.'
