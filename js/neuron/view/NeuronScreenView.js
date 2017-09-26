f3cc1c42417dd0d37ead4d8163a381cb666ec4fe
Chris Malley <cmalley@pixelzoom.com>
2017-09-26 11:41:46 -0600

    replace PHET_LOGO_YELLOW with BUTTON_YELLOW for buttons, https://github.com/phetsims/scenery-phet/issues/337

--- js/neuron/view/NeuronScreenView.js
+++ js/neuron/view/NeuronScreenView.js
@@ -259,7 +259,7 @@ define( function( require ) {
     var stimulateNeuronButton = new RectangularPushButton( {
       content: new MultiLineText( stimulateNeuronString, { font: BUTTON_FONT } ),
       listener: function() { self.neuronModel.initiateStimulusPulse(); },
-      baseColor: PhetColorScheme.PHET_LOGO_YELLOW,
+      baseColor: PhetColorScheme.BUTTON_YELLOW,
       right: worldNodeClipArea.bounds.maxX,
       centerY: centerYForLowerControls,
       minWidth: 50,
