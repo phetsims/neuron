b28eb6edd429db2316a05290619c2e9d264a4193
pixelzoom <cmalley@pixelzoom.com>
2020-09-25 15:54:45 -0600

    convert Screen subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/neuron/view/NeuronScreen.js
+++ js/neuron/view/NeuronScreen.js
@@ -9,26 +9,20 @@
 
 import Property from '../../../../axon/js/Property.js';
 import Screen from '../../../../joist/js/Screen.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import neuron from '../../neuron.js';
 import NeuronClockModelAdapter from '../model/NeuronClockModelAdapter.js';
 import NeuronModel from '../model/NeuronModel.js';
 import NeuronScreenView from './NeuronScreenView.js';
 
-/**
- * Creates the model and view for the NeuronScreen
- * @constructor
- */
-function NeuronScreen() {
-
-  Screen.call( this,
-    function() { return new NeuronClockModelAdapter( new NeuronModel() ); }, // clock model adapter provides constant ticks to model
-    function( model ) { return new NeuronScreenView( model ); },
-    { backgroundColorProperty: new Property( '#ccfefa' ) }
-  );
+class NeuronScreen extends Screen {
+  constructor() {
+    super(
+      function() { return new NeuronClockModelAdapter( new NeuronModel() ); }, // clock model adapter provides constant ticks to model
+      function( model ) { return new NeuronScreenView( model ); },
+      { backgroundColorProperty: new Property( '#ccfefa' ) }
+    );
+  }
 }
 
 neuron.register( 'NeuronScreen', NeuronScreen );
-
-inherit( Screen, NeuronScreen );
 export default NeuronScreen;
