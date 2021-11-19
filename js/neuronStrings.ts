55bb6d0b95b926ad64d8b8fe7d10e5ffd07ca95b
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:12 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/neuronStrings.ts
@@ -0,0 +1,52 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import neuron from './neuron.js';
+
+type StringsType = {
+  'neuron': {
+    'title': string
+  },
+  'stimulateNeuron': string,
+  'legend': string,
+  'sodiumIon': string,
+  'potassiumIon': string,
+  'sodiumGatedChannel': string,
+  'potassiumGatedChannel': string,
+  'sodiumLeakChannel': string,
+  'potassiumLeakChannel': string,
+  'allIons': string,
+  'potentialChart': string,
+  'charges': string,
+  'concentrations': string,
+  'chartTitle': string,
+  'chartYAxisLabel': string,
+  'chartXAxisLabel': string,
+  'chartClear': string,
+  'showLegend': string,
+  'units': {
+    'mM': string
+  },
+  'concentrationReadoutPattern': {
+    '0label': {
+      '1value': {
+        '2units': string
+      }
+    }
+  },
+  'potassiumChemicalSymbol': string,
+  'sodiumChemicalSymbol': string,
+  'fastForward': string,
+  'normal': string,
+  'slowMotion': string
+};
+
+const neuronStrings = getStringModule( 'NEURON' ) as StringsType;
+
+neuron.register( 'neuronStrings', neuronStrings );
+
+export default neuronStrings;
