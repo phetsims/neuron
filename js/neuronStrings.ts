7f245af24100af227bd2bd57d094e897236b5477
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:42 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/neuronStrings.ts
+++ js/neuronStrings.ts
@@ -5,44 +5,70 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import neuron from './neuron.js';
 
 type StringsType = {
   'neuron': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'stimulateNeuron': string;
+  'stimulateNeuronProperty': TReadOnlyProperty<string>;
   'legend': string;
+  'legendProperty': TReadOnlyProperty<string>;
   'sodiumIon': string;
+  'sodiumIonProperty': TReadOnlyProperty<string>;
   'potassiumIon': string;
+  'potassiumIonProperty': TReadOnlyProperty<string>;
   'sodiumGatedChannel': string;
+  'sodiumGatedChannelProperty': TReadOnlyProperty<string>;
   'potassiumGatedChannel': string;
+  'potassiumGatedChannelProperty': TReadOnlyProperty<string>;
   'sodiumLeakChannel': string;
+  'sodiumLeakChannelProperty': TReadOnlyProperty<string>;
   'potassiumLeakChannel': string;
+  'potassiumLeakChannelProperty': TReadOnlyProperty<string>;
   'allIons': string;
+  'allIonsProperty': TReadOnlyProperty<string>;
   'potentialChart': string;
+  'potentialChartProperty': TReadOnlyProperty<string>;
   'charges': string;
+  'chargesProperty': TReadOnlyProperty<string>;
   'concentrations': string;
+  'concentrationsProperty': TReadOnlyProperty<string>;
   'chartTitle': string;
+  'chartTitleProperty': TReadOnlyProperty<string>;
   'chartYAxisLabel': string;
+  'chartYAxisLabelProperty': TReadOnlyProperty<string>;
   'chartXAxisLabel': string;
+  'chartXAxisLabelProperty': TReadOnlyProperty<string>;
   'chartClear': string;
+  'chartClearProperty': TReadOnlyProperty<string>;
   'showLegend': string;
+  'showLegendProperty': TReadOnlyProperty<string>;
   'units': {
     'mM': string;
+    'mMProperty': TReadOnlyProperty<string>;
   };
   'concentrationReadoutPattern': {
     '0label': {
       '1value': {
         '2units': string;
+        '2unitsProperty': TReadOnlyProperty<string>;
       }
     }
   };
   'potassiumChemicalSymbol': string;
+  'potassiumChemicalSymbolProperty': TReadOnlyProperty<string>;
   'sodiumChemicalSymbol': string;
+  'sodiumChemicalSymbolProperty': TReadOnlyProperty<string>;
   'fastForward': string;
+  'fastForwardProperty': TReadOnlyProperty<string>;
   'normal': string;
+  'normalProperty': TReadOnlyProperty<string>;
   'slowMotion': string;
+  'slowMotionProperty': TReadOnlyProperty<string>;
 };
 
 const neuronStrings = getStringModule( 'NEURON' ) as StringsType;
